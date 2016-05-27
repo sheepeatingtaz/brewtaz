fd0718c7676196d1282acbff521ae551466381c7
Steve Garton <steve.garton@oraclefinance.co.uk>
2016-05-27 14:22:58 +0100

    Try postgres

--- brew/settings.py
+++ brew/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
